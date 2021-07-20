6e10045ed2e3d303ef4d1ab56c0d5f582ccb2a83
Nathan Caraker <33259819+ncaraker@users.noreply.github.com>
2021-07-20 12:24:43 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
 
+// Demo API Endpoint: https://reqres.in/api/users
+// Data to send to API: {"name": "morpheus", "job": "leader"}
+
 getBtn.addEventListener('click', getData);
 postBtn.addEventListener('click', sendData);
